f3d415ee1af20eb38e38929f8cd72619e5c0e38e
Aditya <jusadyee@gmail.com>
2025-05-15 19:53:25 +0545

    Update Clause class to use 'required' modifier for Text property and format code

--- AutoComply.Models/Clause.cs
+++ AutoComply.Models/Clause.cs
@@ -1,8 +1,7 @@
 public class Clause
 {
-
     public int Id { get; set; }
-    public string Text { get; set; }
+    public required string Text { get; set; }
     public int Page { get; set; }
-    public string? Category{ get; set; }
+    public string? Category { get; set; }
 }
